96a4e089e1087444e28206811f37fa360a052986
NumberSir <52584526+NumberSir@users.noreply.github.com>
2023-03-26 17:57:43 +0800

    Update __init__.py

--- nonebot_plugin_helloworld/__init__.py
+++ nonebot_plugin_helloworld/__init__.py
@@ -1,9 +1,19 @@
-from nonebot import on_fullmatch
+from nonebot import on_message
 from nonebot.matcher import Matcher
+from nonebot.adapters.onebot.v11 import MessageEvent
 
-helloworld = on_fullmatch("hello")
+
+helloworld = on_message()
 
 
 @helloworld.handle()
-async def _(matcher: Matcher) -> None:
-    await matcher.send("world")
+async def _(matcher: Matcher, event: MessageEvent) -> None:
+    key = event.message.extract_plain_text()
+    if key in I18N:
+        await matcher.send(I18N[key])
+
+
+I18N = {
+    "hello": "world",
+    "你好": "世界"
+}
