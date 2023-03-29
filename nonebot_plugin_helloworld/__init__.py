a4cfdb2e89320359348edc918167f9efd16d554e
NumberSir <52584526+NumberSir@users.noreply.github.com>
2023-03-29 08:28:08 +0800

    优化实现
    加了 rule, priority 和 block

--- nonebot_plugin_helloworld/__init__.py
+++ nonebot_plugin_helloworld/__init__.py
@@ -1,16 +1,21 @@
 from nonebot import on_message
 from nonebot.matcher import Matcher
+from nonebot.params import EventPlainText
 from nonebot.adapters.onebot.v11 import MessageEvent
 
 
-helloworld = on_message()
+async def rule_check_head(text: str = EventPlainText()) -> bool:
+    return text in I18N
+
+
+
+helloworld = on_message(rule=rule_check_head, priority=5, block=False)
 
 
 @helloworld.handle()
 async def _(matcher: Matcher, event: MessageEvent) -> None:
     key = event.message.extract_plain_text()
-    if key in I18N:
-        await matcher.send(I18N[key])
+    await matcher.send(I18N[key])
 
 
 I18N = {
