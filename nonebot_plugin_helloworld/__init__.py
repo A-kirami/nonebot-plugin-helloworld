3cc9016edacf2d921bafa41209f8a194a6fc2391
student_2333 <lgc2333@126.com>
2023-05-24 19:23:22 +0800

    重构实现 (#5)
    Co-authored-by: Akirami <66513481+A-kirami@users.noreply.github.com>

--- nonebot_plugin_helloworld/__init__.py
+++ nonebot_plugin_helloworld/__init__.py
@@ -1,19 +1,19 @@
-from nonebot import on_message
+from typing import NoReturn
+
+from nonebot import on_fullmatch
+from nonebot.internal.adapter import Event
 from nonebot.matcher import Matcher
-from nonebot.adapters.onebot.v11 import MessageEvent
-
-
-helloworld = on_message()
-
-
-@helloworld.handle()
-async def _(matcher: Matcher, event: MessageEvent) -> None:
-    key = event.message.extract_plain_text()
-    if key in I18N:
-        await matcher.send(I18N[key])
-
 
 I18N = {
     "hello": "world",
-    "你好": "世界"
+    "你好": "世界",
 }
+
+
+helloworld = on_fullmatch(tuple(I18N), ignorecase=True)
+
+
+@helloworld.handle()
+async def _(matcher: Matcher, event: Event) -> NoReturn:
+    key = event.get_plaintext().lower()
+    await matcher.finish(I18N[key])
