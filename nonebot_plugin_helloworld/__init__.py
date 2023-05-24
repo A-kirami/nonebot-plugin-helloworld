80e55d8033df0c2902f7bcb58c0c5230fb3bcd47
student_2333 <lgc2333@126.com>
2023-05-24 17:35:18 +0800

    有阻塞风险

--- nonebot_plugin_helloworld/__init__.py
+++ nonebot_plugin_helloworld/__init__.py
@@ -1,19 +1,27 @@
 from nonebot import on_message
-from nonebot.matcher import Matcher
 from nonebot.adapters.onebot.v11 import MessageEvent
-
-
-helloworld = on_message()
-
-
-@helloworld.handle()
-async def _(matcher: Matcher, event: MessageEvent) -> None:
-    key = event.message.extract_plain_text()
-    if key in I18N:
-        await matcher.send(I18N[key])
-
+from nonebot.matcher import Matcher
+from nonebot.typing import T_State
 
 I18N = {
     "hello": "world",
-    "你好": "世界"
+    "你好": "世界",
 }
+
+
+async def hello_rule(event: MessageEvent, state: T_State) -> bool:
+    key = event.message.extract_plain_text()
+    for k, v in I18N.items():
+        if key == k:
+            state["reply"] = v
+            return True
+    return False
+
+
+helloworld = on_message(rule=hello_rule)
+
+
+@helloworld.handle()
+async def _(matcher: Matcher, state: T_State) -> None:
+    reply: str = state["reply"]
+    await matcher.send(reply)
